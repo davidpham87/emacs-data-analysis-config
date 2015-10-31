07112ad42706d81868419cbf6fee0e33b5242aab
David Pham <davidpham87@gmail.com>
2015-10-31 17:19:45 +0100

    fix: Font set correctly as a hook for frame

--- packs/emacs-appearance/config/appearance.el
+++ packs/emacs-appearance/config/appearance.el
@@ -1,27 +1,16 @@
 ;; Set default font
-(when (member "Source Code Pro" (font-family-list))
-  (add-to-list 'default-frame-alist '(font . "Source Code Pro-11"))
-  (set-frame-font  "Source Code Pro-11" nil t))
+(defun set-source-code-pro-font (&rest frame)
+  "Set Adobe Source Code Pro font if exists"
+  (when (member "Source Code Pro" (font-family-list))
+    (set-frame-font "Source Code Pro-11" nil t)))
+
+(add-hook 'after-make-frame-functions 'set-source-code-pro-font t)
 
 (add-to-list 'default-frame-alist '(height . 70))
 (add-to-list 'default-frame-alist '(width . 81))
 (setq-default fill-column 79)           ; Automatic width formatting with M-q
 
-;; (defun set-frame-width-interactive (arg)
-;;   "Interactive version of frame width"
-;;   (interacive "p")
-;;   (set-frame-width (selected-frame) arg))
-
 (menu-bar-mode 1) ; In order to see the menu-bar
-
-;; Autocompletion ; Not sure the following is necessary with emacs-live
-;; (require 'auto-complete-config)
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d//ac-dict")
-;; (ac-config-default)
-;; (define-key ac-completing-map "\t" 'ac-complete)
-;; (define-key ac-completing-map "\r" nil)
-;; (define-key ac-completing-map [tab] 'ac-complete)
-;; (define-key ac-completing-map [return] nil)
 
 ;; AutoComplete color
 (set-face-attribute 'ac-candidate-face nil :background "#00222c"
@@ -31,7 +20,7 @@
 (set-face-attribute 'popup-tip-face nil :background "#003A4E"
                     :foreground "light gray")
 
-;;; Set default Tab on 2
+;;; Set default Tab on 2 (Erased in python mode)
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
 (setq indent-line-function 'insert-tab)
