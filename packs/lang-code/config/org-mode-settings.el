a54fedd67d01ebba261da2279f00b9dabe810d0d
David Pham <davidpham87@gmail.com>
2015-11-24 19:09:42 +0100

    feat: Better org-style with theme setup

--- packs/lang-code/config/org-mode-settings.el
+++ packs/lang-code/config/org-mode-settings.el
@@ -1,7 +1,18 @@
-(defun disable-yas-tab-key ()
-  "Disable the tab expension key in org-mode. Temporary hack"
-  (define-key yas-minor-mode-map [(tab)] nil)
-  (define-key yas-minor-mode-map (kbd "TAB") nil)
-  (define-key yas-minor-mode-map (kbd "<tab>") nil))
+;; (defun disable-yas-tab-key ()
+;;   "Disable the tab expension key in org-mode. Temporary hack"
+;;   (define-key yas-minor-mode-map [(tab)] nil)
+;;   (define-key yas-minor-mode-map (kbd "TAB") nil)
+;;   (define-key yas-minor-mode-map (kbd "<tab>") nil))
 
-(add-hook 'org-mode-hook 'disable-yas-tab-key)
+;; (add-hook 'org-mode-hook 'disable-yas-tab-key)
+(setq org-src-fontify-natively t)
+
+(defun org-html-generate-style-link ()
+  "Generate a link for css"
+  (interactive)
+  (let ((p (concat "#+SETUPFILE: " (concat user-emacs-directory
+                                             "packs/lang-code/lib/org-html-themes"
+                                             "/setup/theme-readtheorg.setup"))))
+    (print p)
+    p))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
