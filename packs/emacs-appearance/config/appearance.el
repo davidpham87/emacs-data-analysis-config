e07743a9bedc9e6ada5e3a91ced4b1b548e6645b
David Pham <davidpham87@gmail.com>
2015-11-25 17:56:34 +0100

    Remove auto-complete style

--- packs/emacs-appearance/config/appearance.el
+++ packs/emacs-appearance/config/appearance.el
@@ -9,12 +9,12 @@
 (menu-bar-mode 1) ; In order to see the menu-bar
 
 ;; AutoComplete color
-(set-face-attribute 'ac-candidate-face nil :background "#00222c"
-                    :foreground "light gray")
-(set-face-attribute 'ac-selection-face nil :background "SteelBlue4"
-                    :foreground "white")
-(set-face-attribute 'popup-tip-face nil :background "#003A4E"
-                    :foreground "light gray")
+;; (set-face-attribute 'ac-candidate-face nil :background "#00222c"
+;;                     :foreground "light gray")
+;; (set-face-attribute 'ac-selection-face nil :background "SteelBlue4"
+;;                     :foreground "white")
+;; (set-face-attribute 'popup-tip-face nil :background "#003A4E"
+;;                     :foreground "light gray")
 
 
 ;;; Set default Tab on 2 (Erased in python mode)
